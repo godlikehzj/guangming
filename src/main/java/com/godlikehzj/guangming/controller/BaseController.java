d5627e6427761360a0adbaeaf4e3b72b2fdebe0f
godlikehzj <godlikehzj@qq.com>
2016-11-15 17:50:04 +0800

    add files

---
+++ src/main/java/com/godlikehzj/guangming/controller/BaseController.java
@@ -0,0 +1,7 @@
+package com.godlikehzj.guangming.controller;
+
+/**
+ * Created by zhijunhu on 2016/11/15.
+ */
+public class BaseController {
+}
---
+++ src/main/java/com/godlikehzj/guangming/service/mapper/CustomerMapper.java
@@ -0,0 +1,7 @@
+package com.godlikehzj.guangming.service.mapper;
+
+/**
+ * Created by zhijunhu on 2016/11/15.
+ */
+public interface CustomerMapper {
+}
